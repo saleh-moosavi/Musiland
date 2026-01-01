7ff2f74544d11d56b4f01dbb794426ab6b5b77e8
Saleh Moosavi <salehmoosavi77@gmail.com>
2026-01-01 09:11:41 -0800

    refactor : change folders and structure of project
    - delete back folder to move api logic to api routes in front folder
    - move files in front folder to main folder

--- back/src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { loginUser, registerUser } from "@controllers/auth.js";
-
-const authRouter = Router();
-
-authRouter.post("/login", loginUser);
-authRouter.post("/register", registerUser);
-
-export { authRouter };
--- back/src/routes/like.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { toggleLike, getUserLiked } from "@controllers/like.js";
-
-const likeRouter = Router();
-
-likeRouter.route("/").post(getUserLiked).put(toggleLike);
-
-export { likeRouter };
