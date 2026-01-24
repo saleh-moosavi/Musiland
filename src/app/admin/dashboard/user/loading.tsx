e1f8c99b1f03f2400e8c05694331681587c5c08a
Saleh Moosavi <salehmoosavi77@gmail.com>
2026-01-24 11:33:52 +0330

    feat : add skeleton loading
    - loading for users in admin page

---
+++ src/app/admin/dashboard/user/loading.tsx
@@ -0,0 +1,14 @@
+import { ImageSkeleton, TextSkeleton } from "@/components/Skeleton";
+
+export default function loading() {
+  return (
+    <section className="h-full w-full flex flex-col justify-start gap-10 dark:text-my-white-low">
+      <TextSkeleton />
+      <article className="grid sm:grid-cols-2 lg:grid-cols-3 *:col-span-1 gap-5 w-farticlel">
+        {[0, 1, 2, 3, 4, 5].map((item) => {
+          return <ImageSkeleton key={item} height="S" />;
+        })}
+      </article>
+    </section>
+  );
+}
