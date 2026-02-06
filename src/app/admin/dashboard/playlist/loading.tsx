7777ea832d13a790cde03ed478c154d319669063
Saleh Moosavi <salehmoosavi77@gmail.com>
2026-02-06 13:20:06 +0330

    feat : add loading
    - loading for album,genre,playlist,singer,song
    - fix user loading

--- src/app/admin/dashboard/playlist/loading.tsx
+++ src/app/admin/dashboard/playlist/loading.tsx
@@ -1,4 +1,4 @@
-import { ImageSkeleton, TextSkeleton } from "@/components/Skeleton";
+import { TextSkeleton } from "@/components/Skeleton";
 
 export default function loading() {
   return (
@@ -6,7 +6,7 @@ export default function loading() {
       <TextSkeleton />
       <article className="grid sm:grid-cols-2 lg:grid-cols-3 *:col-span-1 gap-5">
         {[0, 1, 2, 3, 4, 5].map((item) => {
-          return <ImageSkeleton key={item} height="S" />;
+          return <TextSkeleton key={item} height="M" />;
         })}
       </article>
     </section>
--- src/app/admin/dashboard/song/loading.tsx
+++ src/app/admin/dashboard/song/loading.tsx
@@ -4,8 +4,8 @@ export default function loading() {
   return (
     <section className="h-full w-full flex flex-col justify-start gap-10 dark:text-my-white-low">
       <TextSkeleton />
-      <article className="grid sm:grid-cols-2 lg:grid-cols-3 *:col-span-1 gap-5">
-        {[0, 1, 2, 3, 4, 5].map((item) => {
+      <article className="grid sm:grid-cols-2 *:col-span-1 gap-5 w-full">
+        {[0, 1, 2, 3].map((item) => {
           return <ImageSkeleton key={item} height="S" />;
         })}
       </article>
